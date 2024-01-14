3f7847dcf2b29bb28eb8635fb59cd377a8fd0720
phanupongr <phanupong.ra@gmail.com>
2024-01-14 16:24:35 +0700

    update script

--- src/fetch_sp500.py
+++ src/fetch_sp500.py
@@ -11,26 +11,17 @@ def write_to_csv(file_path: str, table: pd.DataFrame):
         for row in table.itertuples():
             f.write(f'{row.Symbol},{row.Security}\n')
 
-def fetch_existing_sp500_table(file_path: str):
-    pass
-
-def write_to_file(file_path: str, table: pd.DataFrame):
-    pass
-
 def main():
+    ## Extract
     URL: str = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     sp500_table: pd.DataFrame = fetch_sp500_table(URL)
 
-    existing_sp500_table = fetch_existing_sp500_table('sp500.csv')
+    ## Transform
+    # eliminate commas in company names by add double quotes
+    sp500_table['Security'] = sp500_table['Security'].apply(lambda x: f'"{x}"')
 
-    df = pd.read_csv('sp500.csv', on_bad_lines='warn')
-
-    # sp500_table_diff = sp500_table[~sp500_table.Symbol.isin(existing_sp500_table.Symbol)]
-
-    # print(sp500_table_diff)
-
-    # write_to_csv('sp500.csv', sp500_table)
-    # write_to_markdown('README.md', sp500_table)
+    ## Load
+    write_to_csv('sp500.csv', sp500_table)
 
 if __name__ == '__main__':
     main()
